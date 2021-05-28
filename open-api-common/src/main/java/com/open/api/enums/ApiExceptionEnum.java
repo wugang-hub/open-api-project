bd256fe8ddfcfab6aa612004d4960bb7162876f8
wugang <1055503618@qq.com>
2021-05-28 15:57:10 +0800

    完善

--- open-api-common/src/main/java/com/open/api/enums/ApiExceptionEnum.java
+++ open-api-common/src/main/java/com/open/api/enums/ApiExceptionEnum.java
@@ -12,9 +12,9 @@ public enum ApiExceptionEnum implements EnumInterface {
      */
     SYSTEM_ERROR("SYSTEM_ERROR", "系统异常"),
     API_NOT_EXIST("API_NOT_EXIST", "API方法不存在"),
-    INVALID_PUBLIC_PARAM("INVALID_PUBLIC_PARAM", "无效公共参数 >> {0}"),
-    INVALID_REQUEST_ERROR("INVALID_REQUEST_ERROR", " 请求方式 {0} 错误 ! 请使用 {1} 方式"),
-    INVALID_PARAM("INVALID_PARAM", "无效参数 >> 参数[{0}] >> 原因[{1}]"),
+    INVALID_PUBLIC_PARAM("INVALID_PUBLIC_PARAM", "无效公共参数"),
+    INVALID_REQUEST_ERROR("INVALID_REQUEST_ERROR", " 请求方式"),
+    INVALID_PARAM("INVALID_PARAM", "无效参数"),
     INVALID_SIGN("INVALID_SIGN", "无效签名"),
     INVALID_IP("INVALID_IP", "无效IP地址");
     ;
