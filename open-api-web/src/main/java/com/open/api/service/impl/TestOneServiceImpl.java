971bcb59549bb614c7d1b52995b93544031dd868
wugang <1055503618@qq.com>
2021-06-02 10:04:29 +0800

    完善项目结构

--- open-api-web/src/main/java/com/open/api/service/impl/TestOneServiceImpl.java
+++ open-api-web/src/main/java/com/open/api/service/impl/TestOneServiceImpl.java
@@ -2,10 +2,8 @@ package com.open.api.service.impl;
 
 import com.alibaba.fastjson.JSON;
 import com.open.api.annotation.OpenApiService;
-import com.open.api.web.bo.Test1BO;
+import com.open.api.model.Test1BO;
 import com.open.api.service.TestOneService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 /**
